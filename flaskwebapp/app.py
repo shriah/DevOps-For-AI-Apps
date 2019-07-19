61daba31942d5af63fe5ef2829e93ace537677d2
Nithya Samy <nithya0290@gmail.com>
2019-07-19 19:08:37 +0530

    Changed scoring logic and moved to mxnet

--- flaskwebapp/app.py
+++ flaskwebapp/app.py
@@ -1,10 +1,24 @@
 from flask import Flask, request
-import cntk
-from driver import *
+import json
 import time
 
+import mxnet as mx
+path='http://data.mxnet.io/models/imagenet/'
+[mx.test_utils.download(path+'resnet/18-layers/resnet-18-0000.params'),
+ mx.test_utils.download(path+'resnet/18-layers/resnet-18-symbol.json'),
+ mx.test_utils.download(path+'synset.txt')]
+
+ctx = mx.cpu()
+
+sym, arg_params, aux_params = mx.model.load_checkpoint('resnet-18', 0)
+mod = mx.mod.Module(symbol=sym, context=ctx, label_names=None)
+mod.bind(for_training=False, data_shapes=[('data', (1,3,224,224))], 
+         label_shapes=mod._label_shapes)
+mod.set_params(arg_params, aux_params, allow_missing=True)
+with open('synset.txt', 'r') as f:
+    labels = [l.rstrip() for l in f]
+
 app = Flask(__name__)
-init()
 @app.route('/score', methods = ['POST'])
 def scoreRRS():
     """ Endpoint for scoring
@@ -13,23 +27,55 @@ def scoreRRS():
         return Response(json.dumps({}), status= 415, mimetype ='application/json')
     input = request.json['input']
     start = time.time()
-    response = run(input)
+    response = predict(input)
     end = time.time() - start
     dict = {}
     dict['result'] = response
     return json.dumps(dict)
 
 
+
+
+#%matplotlib inline
+import numpy as np
+# define a simple data batch
+from collections import namedtuple
+Batch = namedtuple('Batch', ['data'])
+
+def get_image(url, show=False):
+    # download and show the image. Remove query string from the file name.
+    fname = mx.test_utils.download(url, fname=url.split('/')[-1].split('?')[0])
+    img = mx.image.imread(fname)
+    if img is None:
+        return None
+    # convert into format (batch, RGB, width, height)
+    img = mx.image.imresize(img, 224, 224) # resize
+    img = img.transpose((2, 0, 1)) # Channel first
+    img = img.expand_dims(axis=0) # batchify
+    img = img.astype('float32') # for gpu context
+    return img
+
+
+def predict(url):
+    img = get_image(url, show=True)
+    # compute the predict probabilities
+    mod.forward(Batch([img]))
+    prob = mod.get_outputs()[0].asnumpy()
+    # print the top-5
+    prob = np.squeeze(prob)
+    a = np.argsort(prob)[::-1]
+    dict1=dict()
+    for i in a[0:5]:
+        #print('probability=%f, class=%s' %(prob[i], labels[i]))
+        dict1[labels[i]]=str(prob[i])
+    return dict1
+      
+
 @app.route("/")
 def healthy():
     return "Healthy"
 
-
-# CNTK Version
-@app.route('/version', methods = ['GET'])
-def version_request():
-    return cntk.__version__
-
-
 if __name__ == "__main__":
     app.run(host='0.0.0.0') # Ignore, Development server
+
+
